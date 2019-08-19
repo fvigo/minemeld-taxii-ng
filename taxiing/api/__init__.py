3039774db63c7fef9d4c3d6669f278764c8d5c50
Francesco Vigo <fvigo@paloaltonetworks.com>
2019-08-19 15:03:34 +0200

    STIX2/TAXII2 support: initial push

--- taxiing/api/__init__.py
+++ taxiing/api/__init__.py
@@ -10,4 +10,12 @@ def blueprint():
             '/v1/poll', methods=['POST'], feeds=True, read_write=False
         )(poll)
 
+    if config.get('TAXIING_ENABLE_TAXII2_STIC2_POLL', False):
+        from .v2 import get_apiroot, get_collection, get_collections, get_taxii2_server, get_collection_objects
+
+        bp.route('/v2/', methods=['GET'], feeds=True, read_write=False)(get_apiroot)
+        bp.route('/v2/collections/', methods=['GET'], feeds=True, read_write=False)(get_collections)
+        bp.route('/v2/collections/<collection>/', methods=['GET'], feeds=True, read_write=False)(get_collection)
+        bp.route('/v2/', methods=['GET'], feeds=True, read_write=False)(get_taxii2_server)
+        bp.route('/v2/collections/<collection>/objects/', methods=['GET'], feeds=True, read_write=False)(get_collection_objects)
     return bp
